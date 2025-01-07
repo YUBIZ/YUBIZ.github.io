e996270002748991914aeb07c8e856fd7e6dd2da
YUBIZ <k.sein1016@gmail.com>
2025-01-08 02:57:40 +0900

    게시글 카드 컴포넌트 추가 및 Blog.Components 사용 추가

---
+++ Blog/Components/PostCard.razor.cs
@@ -0,0 +1,26 @@
+﻿using Blog.Models;
+using Blog.Extensions;
+using System.Net.Http.Json;
+
+namespace Blog.Components;
+
+public partial class PostCard(HttpClient httpClient)
+{
+    private string Title { get; set; } = string.Empty;
+    private string Summary { get; set; } = string.Empty;
+    private string[] Tags { get; set; } = [];
+    private DateTime PublishedDateTime { get; set; }
+    private DateTime UpdatedDateTime { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        PostMetadata postMetadata = await httpClient.GetFromYamlFrontMatterAsync<PostMetadata>(Path.ChangeExtension(PostUri, "md"));
+        Title = postMetadata.Title;
+        Summary = postMetadata.Summary;
+        Tags = postMetadata.Tags;
+
+        Commit[]? commitHistory = await httpClient.GetFromJsonAsync<Commit[]>($"commit-history/{Path.ChangeExtension(PostUri, "json")}");
+        PublishedDateTime = (commitHistory?.FirstOrDefault() ?? default).Timestamp;
+        UpdatedDateTime = (commitHistory?.LastOrDefault() ?? default).Timestamp;
+    }
+}
