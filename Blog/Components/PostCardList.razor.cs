cb40bf9759daa73f3e9de2feecb9fb25fc7045d4
YUBIZ <k.sein1016@gmail.com>
2025-01-08 02:58:04 +0900

    게시글 카드 목록 컴포넌트 추가

---
+++ Blog/Components/PostCardList.razor.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Json;
+
+namespace Blog.Components;
+
+public partial class PostCardList(HttpClient httpClient)
+{
+    public IEnumerable<string> PostUris { get; set; } = [];
+
+    protected override async Task OnInitializedAsync()
+    {
+        PostUris = await httpClient.GetFromJsonAsync<string[]>("file-list/posts.json") ?? [];
+    }
+}
