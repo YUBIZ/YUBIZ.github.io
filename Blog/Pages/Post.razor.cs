abfcc2290553a9de3389d1e278e8b1fc79147368
YUBIZ <k.sein1016@gmail.com>
2025-01-09 20:32:27 +0900

    게시글 페이지 추가

---
+++ Blog/Pages/Post.razor.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Blog.Pages;
+
+public partial class Post(StaticResourceService staticResourceService)
+{
+    private string PostUri => $"posts/{PostNameWithCategory}";
+    private string? Title { get; set; }
+    private MarkupString Content { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Title = (await staticResourceService.GetPostMetadataAsync(PostUri)).Title;
+        Content = await staticResourceService.GetPostContentAsync(PostUri);
+    }
+}
