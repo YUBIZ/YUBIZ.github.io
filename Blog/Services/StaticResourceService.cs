daef6371a1deca77871c6ab6f871aebde76f187d
YUBIZ <k.sein1016@gmail.com>
2025-01-09 12:30:28 +0900

    정적 리소스 서비스 추가 및 파일 목록, 커밋 이력, 게시글 메타데이터, 게시글 콘텐츠 비동기 획득 메서드 추가

---
+++ Blog/Services/StaticResourceService.cs
@@ -0,0 +1,32 @@
+﻿using Blog.Extensions;
+using Blog.Models;
+using Markdig;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace Blog.Services;
+
+public class StaticResourceService(HttpClient httpClient)
+{
+    public async Task<string[]> GetFileListAsync(string fileListUri)
+    {
+        return await httpClient.GetFromJsonAsync<string[]>($"file-list/{Path.ChangeExtension(fileListUri, "json")}") ?? [];
+    }
+
+    public async Task<Commit[]> GetCommitHistoryAsync(string commitHistoryUri)
+    {
+        return await httpClient.GetFromJsonAsync<Commit[]>($"commit-history/{Path.ChangeExtension(commitHistoryUri, "json")}") ?? [];
+    }
+
+    public async Task<PostMetadata> GetPostMetadataAsync(string postUri)
+    {
+        return await httpClient.GetFromYamlFrontMatterAsync<PostMetadata>(Path.ChangeExtension(postUri, "md"));
+    }
+
+    public async Task<MarkupString> GetPostContentAsync(string postUri)
+    {
+        string postString = await httpClient.GetStringAsync(Path.ChangeExtension(postUri, "md"));
+        MarkdownPipeline markdownPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().UseYamlFrontMatter().UseBootstrap().Build();
+        return (MarkupString)Markdown.ToHtml(postString, markdownPipeline);
+    }
+}
