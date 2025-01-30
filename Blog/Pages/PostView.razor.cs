e9d0c91c08d970fc608f16f854ab7ae4e628b952
YUBIZ <k.sein1016@gmail.com>
2025-01-31 06:46:48 +0900

    게시글 페이지에 제목, 태그, 게시일 및 수정일 정보를 추가

--- Blog/Pages/PostView.razor.cs
+++ Blog/Pages/PostView.razor.cs
@@ -1,4 +1,5 @@
-﻿using Blog.Services;
+﻿using Blog.Models;
+using Blog.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace Blog.Pages;
@@ -7,11 +8,21 @@ public partial class PostView(StaticResourceService staticResourceService)
 {
     private string PostUri => $"posts/{PostNameWithCategory}";
     private string? Title { get; set; }
+    private string[]? Tags { get; set; }
+    public DateTime PublishedDateTime { get; private set; }
+    public DateTime UpdatedDateTime { get; private set; }
     private MarkupString Content { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
-        Title = (await staticResourceService.GetPostMetadataAsync(PostUri)).Title;
+        PostMetadata postMetadata = await staticResourceService.GetPostMetadataAsync(PostUri);
+        Title = postMetadata.Title;
+        Tags = postMetadata.Tags;
+
+        Commit[]? commitHistory = await staticResourceService.GetCommitHistoryAsync(PostUri);
+        PublishedDateTime = (commitHistory?.FirstOrDefault() ?? default).Timestamp;
+        UpdatedDateTime = (commitHistory?.LastOrDefault() ?? default).Timestamp;
+
         Content = await staticResourceService.GetPostContentAsync(PostUri);
     }
 }
