e85b2f11e54bdc9adafed51551f7d90aad10ad1e
YUBIZ <k.sein1016@gmail.com>
2025-02-22 07:07:01 +0900

    GitHub 서비스에서 커밋 목록을 가져오는 기능 및 YAML 프론트 매터 메타데이터 추출 기능 추가

--- Blog/Services/GitHubService.cs
+++ Blog/Services/GitHubService.cs
@@ -1,6 +1,7 @@
 ﻿using Blog.Models;
 using System.Text;
 using System.Text.Json;
+using YamlDotNet.Serialization;
 
 namespace Blog.Services;
 
@@ -8,12 +9,41 @@ public class GitHubService(HttpClient httpClient, ConfigService configService)
 {
     private GitHubPostsConfig GitHubPostsConfig => configService.GitHubPostsConfig;
 
-    public async Task<string> GetContentAsync(string path, GitHubPostsConfig? gitHubPostsConfig = null)
+    public async Task<Commit[]> GetCommitsAsync(string path, GitHubPostsConfig? gitHubPostsConfig = null)
     {
         gitHubPostsConfig ??= GitHubPostsConfig;
 
         httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(gitHubPostsConfig.UserAgent);
         httpClient.DefaultRequestHeaders.Authorization = new("Bearer", gitHubPostsConfig.AccessToken);
+
+        var response = await httpClient.GetAsync($"https://api.github.com/repos/{gitHubPostsConfig.Owner}/{gitHubPostsConfig.Repository}/commits?sha={gitHubPostsConfig.Branch}&path={path}");
+
+        if (!response.IsSuccessStatusCode) return [];
+
+        return JsonDocument.Parse(await response.Content.ReadAsStringAsync())
+            .RootElement
+            .EnumerateArray()
+            .Select(
+            e => new Commit(
+                e.GetProperty("commit")
+                .GetProperty("message")
+                .GetString() ?? "",
+                e.GetProperty("commit")
+                .GetProperty("author")
+                .GetProperty("date")
+                .GetDateTime()
+                .ToLocalTime()
+                )
+            )
+            .ToArray();
+    }
+
+    public async Task<string> GetContentAsync(string path, GitHubPostsConfig? gitHubPostsConfig = null)
+    {
+        gitHubPostsConfig ??= GitHubPostsConfig;
+
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(gitHubPostsConfig.UserAgent);
+        httpClient.DefaultRequestHeaders.Authorization = new("Bearer", GitHubPostsConfig.AccessToken);
 
         var response = await httpClient.GetAsync($"https://api.github.com/repos/{gitHubPostsConfig.Owner}/{gitHubPostsConfig.Repository}/contents/{path}?ref={gitHubPostsConfig.Branch}");
 
@@ -30,5 +60,18 @@ public class GitHubService(HttpClient httpClient, ConfigService configService)
     public async Task<string[]> GetPostListAsync()
     {
         return JsonSerializer.Deserialize<string[]>(await GetContentAsync(GitHubPostsConfig.PostListFilePath)) ?? [];
+    }
+
+    public async Task<PostMetadata> GetPostMetadataAsync(string postUri)
+    {
+        string[] lines = (await GetContentAsync(postUri)).Split('\n')
+                                                         .Select(static line => line.Trim())
+                                                         .ToArray();
+        if (lines.Length == 0 || lines.First() != "---") throw new("YAML Front Matter의 시작을 찾을 수 없습니다.");
+
+        int lastIndex = Array.FindIndex(lines, 1, static line => line == "---");
+        if (lastIndex == -1) throw new("YAML Front Matter의 끝을 찾을 수 없습니다.");
+
+        return new Deserializer().Deserialize<PostMetadata>(string.Join('\n', lines[1..lastIndex]));
     }
 }
