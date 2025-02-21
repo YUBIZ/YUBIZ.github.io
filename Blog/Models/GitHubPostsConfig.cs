e366ad3ef11713ca21208140d319e0386423ccbc
YUBIZ <k.sein1016@gmail.com>
2025-02-22 05:08:38 +0900

    GitHub 게시글 설정 모델 및 설정 서비스 추가

---
+++ Blog/Models/GitHubPostsConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.Models;
+
+public class GitHubPostsConfig
+{
+    public required string AccessToken { get; set; }
+    public required string Owner { get; set; }
+    public required string Repository { get; set; }
+    public required string Branch { get; set; }
+    public required string PostListFilePath { get; set; }
+}
---
+++ Blog/Services/ConfigService.cs
@@ -0,0 +1,9 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Blog.Services;
+
+public class ConfigService(WebAssemblyHostConfiguration configuration)
+{
+    public GitHubPostsConfig GitHubPostsConfig { get; } = configuration.GetSection("GitHub:Posts").Get<GitHubPostsConfig>() ?? throw new ArgumentNullException(nameof(GitHubPostsConfig));
+}
