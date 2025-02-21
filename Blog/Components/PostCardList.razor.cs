ec6af44e804258d56fb18dc2156f5cdcb1283233
YUBIZ <k.sein1016@gmail.com>
2025-02-22 05:09:18 +0900

    GitHub 서비스에서 게시글 목록을 비동기적으로 가져오는 기능 추가

--- Blog/Components/PostCardList.razor.cs
+++ Blog/Components/PostCardList.razor.cs
@@ -3,11 +3,12 @@ using System.Text.Json;
 
 namespace Blog.Components;
 
-public partial class PostCardList
+public partial class PostCardList(GitHubService gitHubService)
 {
     public IEnumerable<string> PostUris { get; set; } = [];
 
     protected override async Task OnInitializedAsync()
     {
+        PostUris = await gitHubService.GetPostListAsync();
     }
 }
