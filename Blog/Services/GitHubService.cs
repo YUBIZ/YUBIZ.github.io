a015cc21770bc158f4f5a465316ab2d142e56adc
YUBIZ <k.sein1016@gmail.com>
2025-03-11 17:29:52 +0900

    GitHubService 클래스 추가 및 원시 데이터 가져오기 메서드 구현

---
+++ Blog/Services/GitHubService.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Json;
+
+namespace Blog.Services;
+
+public class GitHubService
+{
+    private readonly HttpClient httpClient = new() { BaseAddress = new Uri("https://raw.githubusercontent.com") };
+
+    public async Task<HttpResponseMessage> GetRawAsync(string owner, string repo, string @ref, string path)
+        => await httpClient.GetAsync(string.Join("/", owner, repo, @ref, path));
+
+    public async Task<string> GetRawStringAsync(string owner, string repo, string @ref, string path)
+        => await httpClient.GetStringAsync(string.Join("/", owner, repo, @ref, path));
+
+    public async Task<T?> GetRawFromJsonAsync<T>(string owner, string repo, string @ref, string path)
+        => await httpClient.GetFromJsonAsync<T>(string.Join("/", owner, repo, @ref, path));
+}
