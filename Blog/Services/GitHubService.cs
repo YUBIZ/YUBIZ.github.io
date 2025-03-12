154e119649359c4b1b105ed1533bc7997acc941b
YUBIZ <k.sein1016@gmail.com>
2025-03-12 13:24:20 +0900

    경로 결합 방식을 문자열 보간으로 변경

--- Blog/Services/GitHubService.cs
+++ Blog/Services/GitHubService.cs
@@ -7,11 +7,11 @@ public class GitHubService
     private readonly HttpClient httpClient = new() { BaseAddress = new Uri("https://raw.githubusercontent.com") };
 
     public async Task<HttpResponseMessage> GetRawAsync(string owner, string repo, string @ref, string path)
-        => await httpClient.GetAsync(string.Join("/", owner, repo, @ref, path));
+        => await httpClient.GetAsync($"{owner}/{repo}/{@ref}/{path}");
 
     public async Task<string> GetRawStringAsync(string owner, string repo, string @ref, string path)
-        => await httpClient.GetStringAsync(string.Join("/", owner, repo, @ref, path));
+        => await httpClient.GetStringAsync($"{owner}/{repo}/{@ref}/{path}");
 
     public async Task<T?> GetRawFromJsonAsync<T>(string owner, string repo, string @ref, string path)
-        => await httpClient.GetFromJsonAsync<T>(string.Join("/", owner, repo, @ref, path));
+        => await httpClient.GetFromJsonAsync<T>($"{owner}/{repo}/{@ref}/{path}");
 }
