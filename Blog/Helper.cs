f061f0fc33168cff9b7d762dddbfb1c0cb691522
YUBIZ <k.sein1016@gmail.com>
2025-03-14 12:21:59 +0900

    Helper 정적 클래스 추가 및 YamlFrontMatter 데이터 추출 메서드 구현

---
+++ Blog/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using YamlDotNet.Serialization;
+
+namespace Blog;
+
+public static partial class Helper
+{
+    public static T? GetFromYamlFrontMatter<T>(string input)
+    {
+        Match result = YamlFrontMatterRegex().Match(input);
+        return result.Success ? new Deserializer().Deserialize<T>(result.Groups[1].Value) : default;
+    }
+
+    [GeneratedRegex(@"^---(.*?)---", RegexOptions.Singleline)]
+    private static partial Regex YamlFrontMatterRegex();
+}
