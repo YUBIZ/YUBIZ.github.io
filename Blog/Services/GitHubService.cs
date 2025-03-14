f37cd50d0c43d3b6c0f8c27fc3a7dbbd5e8be593
YUBIZ <k.sein1016@gmail.com>
2025-03-14 16:46:38 +0900

    GitHubService에 Owner, Repo, Ref 속성 추가 및 원시 데이터 가져오기 메서드 오버로딩

--- Blog/Services/GitHubService.cs
+++ Blog/Services/GitHubService.cs
@@ -4,14 +4,27 @@ namespace Blog.Services;
 
 public class GitHubService
 {
-    private readonly HttpClient httpClient = new() { BaseAddress = new Uri("https://raw.githubusercontent.com") };
+    public string? Owner { get; set; }
+    public string? Repo { get; set; }
+    public string? Ref { get; set; }
 
+    private readonly HttpClient httpClient = new() { BaseAddress = new Uri("https://raw.githubusercontent.com") };
+        
     public async Task<HttpResponseMessage> GetRawAsync(string owner, string repo, string @ref, string path)
         => await httpClient.GetAsync($"{owner}/{repo}/{@ref}/{path}");
+
+    public async Task<HttpResponseMessage> GetRawAsync(string path)
+        => await GetRawAsync(Owner!, Repo!, Ref!, path);
 
     public async Task<string> GetRawStringAsync(string owner, string repo, string @ref, string path)
         => await httpClient.GetStringAsync($"{owner}/{repo}/{@ref}/{path}");
 
+    public async Task<string> GetRawStringAsync(string path)
+        => await GetRawStringAsync(Owner!, Repo!, Ref!, path);
+
     public async Task<T?> GetRawFromJsonAsync<T>(string owner, string repo, string @ref, string path)
         => await httpClient.GetFromJsonAsync<T>($"{owner}/{repo}/{@ref}/{path}");
+
+    public async Task<T?> GetRawFromJsonAsync<T>(string path)
+        => await GetRawFromJsonAsync<T>(Owner!, Repo!, Ref!, path);
 }
