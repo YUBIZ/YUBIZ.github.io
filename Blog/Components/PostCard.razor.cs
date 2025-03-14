707743513a48e25e6e1054a7cb15e8c4a22ac0a5
YUBIZ <k.sein1016@gmail.com>
2025-03-14 22:15:23 +0900

    PostCard 컴포넌트 추가

---
+++ Blog/Components/PostCard.razor.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Models;
+using Blog.Services;
+
+namespace Blog.Components;
+
+public partial class PostCard(GitHubService gitHubService)
+{
+    private string? thumbnailUri;
+
+    protected override async Task OnInitializedAsync()
+    {
+        Document document = await gitHubService.GetRawFromYamlFrontMatterAsync<Document>(PostUri);
+
+        thumbnailUri = !string.IsNullOrEmpty(document.ThumbnailUri) && document.ThumbnailUri.StartsWith('/')
+            ? $"{GitHubService.RawBaseAdddress}/{gitHubService.Owner}/{gitHubService.Repo}/{gitHubService.Ref}/{document.ThumbnailUri}"
+            : document.ThumbnailUri;
+    }
+}
