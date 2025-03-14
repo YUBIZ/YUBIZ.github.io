bb1f47d7ee799e87326714bf04c28b8459e4ad56
YUBIZ <k.sein1016@gmail.com>
2025-03-14 22:15:41 +0900

    CategoryView/Docs 페이지 추가

---
+++ Blog/Pages/CategoryView/Docs.razor.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Services;
+
+namespace Blog.Pages.CategoryView;
+
+public partial class Docs(GitHubService gitHubService)
+{
+    private string[]? docsList;
+
+    private string[]? SubDocsList => docsList?.Where(v => v.StartsWith($"Docs/{CategoryUri}")).ToArray();
+
+    protected override async Task OnInitializedAsync()
+    {
+        docsList = await gitHubService.GetRawFromJsonAsync<string[]>("DocsList.json");
+    }
+}
