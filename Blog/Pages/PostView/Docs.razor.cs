b1b30a16da687bd4c57080526bf8422011c370d8
YUBIZ <k.sein1016@gmail.com>
2025-03-14 22:15:56 +0900

    PostView/Docs 페이지 추가

---
+++ Blog/Pages/PostView/Docs.razor.cs
@@ -0,0 +1,25 @@
+﻿using Blog.Models;
+using Blog.Services;
+using Markdig;
+using Microsoft.AspNetCore.Components;
+
+namespace Blog.Pages.PostView;
+
+public partial class Docs(GitHubService gitHubService)
+{
+    private string Title => Path.GetFileNameWithoutExtension(PostUri);
+
+    private Document document;
+
+    private MarkupString content;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        var raw = await gitHubService.GetRawStringAsync($"Docs/{PostUri}");
+
+        document = Helper.DeserializeYamlFrontMatter<Document>(raw);
+
+        MarkdownPipeline markdownPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().UseYamlFrontMatter().Build();
+        content = (MarkupString)Markdown.ToHtml(raw, markdownPipeline);
+    }
+}
