cb7a56384d25f22354dd26a0f25d4c11df19edd7
YUBIZ <k.sein1016@gmail.com>
2025-03-15 10:02:12 +0900

    GitHubService에 Params와 RawBaseAddressWithParams 속성 추가 및 메서드 간소화

--- Blog/Services/GitHubService.cs
+++ Blog/Services/GitHubService.cs
@@ -4,35 +4,24 @@ namespace Blog.Services;
 
 public class GitHubService
 {
-    public string? Owner { get; set; }
-    public string? Repo { get; set; }
-    public string? Ref { get; set; }
-
+    public required string Owner { get; init; }
+    public required string Repo { get; init; }
+    public required string Ref { get; init; }
     public static string RawBaseAdddress => "https://raw.githubusercontent.com";
+    public string Params => $"{Owner}/{Repo}/{Ref}";
+    public string RawBaseAdddressWithParams => $"{RawBaseAdddress}/{Params}";
 
     private readonly HttpClient httpClient = new() { BaseAddress = new Uri("https://raw.githubusercontent.com") };
 
-    public async Task<HttpResponseMessage> GetRawAsync(string owner, string repo, string @ref, string path)
-        => await httpClient.GetAsync($"{owner}/{repo}/{@ref}/{path}");
-
     public async Task<HttpResponseMessage> GetRawAsync(string path)
-        => await GetRawAsync(Owner!, Repo!, Ref!, path);
-
-    public async Task<string> GetRawStringAsync(string owner, string repo, string @ref, string path)
-        => await httpClient.GetStringAsync($"{owner}/{repo}/{@ref}/{path}");
+        => await httpClient.GetAsync($"{Params}/{path}");
 
     public async Task<string> GetRawStringAsync(string path)
-        => await GetRawStringAsync(Owner!, Repo!, Ref!, path);
-
-    public async Task<T?> GetRawFromJsonAsync<T>(string owner, string repo, string @ref, string path)
-        => await httpClient.GetFromJsonAsync<T>($"{owner}/{repo}/{@ref}/{path}");
+        => await httpClient.GetStringAsync($"{Params}/{path}");
 
     public async Task<T?> GetRawFromJsonAsync<T>(string path)
-        => await GetRawFromJsonAsync<T>(Owner!, Repo!, Ref!, path);
-
-    public async Task<T?> GetRawFromYamlFrontMatterAsync<T>(string owner, string repo, string @ref, string path)
-        => Helper.DeserializeYamlFrontMatter<T>(await GetRawStringAsync(owner, repo, @ref, path));
+        => await httpClient.GetFromJsonAsync<T>($"{Params}/{path}");
 
     public async Task<T?> GetRawFromYamlFrontMatterAsync<T>(string path)
-        => await GetRawFromYamlFrontMatterAsync<T>(Owner!, Repo!, Ref!, path);
+        => Helper.DeserializeYamlFrontMatter<T>(await GetRawStringAsync(path));
 }
