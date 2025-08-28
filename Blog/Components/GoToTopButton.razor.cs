9f50071ddd3e5b007eeb9fed3eadfad108ef646a
YUBIZ <k.sein1016@gmail.com>
2025-08-28 15:12:49 +0900

    Footer 및 GoToTopButton 컴포넌트 추가, MainLayout에서 사이드바 및 관련 코드 제거

---
+++ Blog/Components/GoToTopButton.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+
+namespace Blog.Components;
+
+public partial class GoToTopButton(IJSRuntime jsRuntime)
+{
+    private async Task GoToTop()
+    {
+        await jsRuntime.InvokeVoidAsync(
+            "window.scrollTo",
+            new { top = 0, behavior = "smooth" }
+        );
+    }
+}
