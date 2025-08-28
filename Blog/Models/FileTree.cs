0f60ae30eb7de30611b773e4b6d2cf790a483738
YUBIZ <k.sein1016@gmail.com>
2025-08-29 05:04:12 +0900

    FileTree 모델에 EnumerateFiles 메서드 추가

--- Blog/Models/FileTree.cs
+++ Blog/Models/FileTree.cs
@@ -1,3 +1,19 @@
 ﻿namespace Blog.Models;
 
-public readonly record struct FileTree(string Name, FileTree[] SubTrees, string[] Files);
+public readonly record struct FileTree(string Name, FileTree[] SubTrees, string[] Files)
+{
+    public IEnumerable<string> EnumerateFiles()
+    {
+        return EnumerateFilesInternal(Name);
+    }
+
+    private IEnumerable<string> EnumerateFilesInternal(string basePath)
+    {
+        foreach (var subTree in SubTrees)
+        {
+            foreach (var file in subTree.EnumerateFilesInternal( Path.Combine(basePath, subTree.Name))) yield return file;
+        }
+
+        foreach (var file in Files) yield return Path.Combine(basePath, file);
+    }
+}
