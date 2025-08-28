e3988626a170e6cebb91311150b61ae2cd217d2e
YUBIZ <k.sein1016@gmail.com>
2025-08-29 00:27:01 +0900

    게시글을 수직으로 배치 및 우측에 바로가기 및 DirectoryTreeView 추가

--- Blog/Pages/CategoryView/Docs.razor.cs
+++ Blog/Pages/CategoryView/Docs.razor.cs
@@ -1,15 +1,18 @@
-﻿using Blog.Services;
+﻿using Blog.Models;
+using Blog.Services;
 
 namespace Blog.Pages.CategoryView;
 
 public partial class Docs(GitHubService gitHubService)
 {
     private string[]? docsList;
+    private FileTree docsTree;
 
     private string[]? SubDocsList => docsList?.Where(v => v.StartsWith($"Docs/{CategoryUri}")).ToArray();
 
     protected override async Task OnInitializedAsync()
     {
+        docsTree = await gitHubService.GetRawFromJsonAsync<FileTree>("DocsTree.json");
         docsList = await gitHubService.GetRawFromJsonAsync<string[]>("DocsList.json");
     }
 }
