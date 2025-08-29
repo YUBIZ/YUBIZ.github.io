4bb2de6c27387fadf94e920fe74ba36fc5e392c3
YUBIZ <k.sein1016@gmail.com>
2025-08-30 07:15:11 +0900

    CategoryView.Docs 페이지 이름을 Document로 변경 및 내부 로직을 DocumentFilePathWithCommitHistoryCollection.json을 활용하는 코드로 변경

---
+++ Blog/Pages/CategoryView/Document.razor.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Models;
+using Blog.Services;
+
+namespace Blog.Pages.CategoryView;
+
+public partial class Document(GitHubService gitHubService)
+{
+    private FileTree documentFileTree;
+
+    private FilePathWithCommitHistory[]? documentFilePathWithCommitHistoryCollection;
+
+    protected override async Task OnInitializedAsync()
+    {
+        documentFileTree = await gitHubService.GetRawFromJsonAsync<FileTree>("DocumentFileTree.json");
+        documentFilePathWithCommitHistoryCollection = await gitHubService.GetRawFromJsonAsync<FilePathWithCommitHistory[]>("DocumentFilePathWithCommitHistoryCollection.json");
+    }
+}
