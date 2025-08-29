ef0d79cf21c576e13336fd3a405f857c5dd1eff4
YUBIZ <k.sein1016@gmail.com>
2025-08-30 01:00:18 +0900

    포스트 카드를 DocumentCard로 이름 변경 및 컴포넌트 스타일 수정

---
+++ Blog/Components/DocumentCard.razor.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Models;
+using Blog.Services;
+
+namespace Blog.Components;
+
+public partial class DocumentCard(GitHubService gitHubService)
+{
+    private DocumentMetadata documentMetadata;
+
+    protected override async Task OnInitializedAsync()
+    {
+        documentMetadata = await gitHubService.GetRawFromYamlFrontMatterAsync<DocumentMetadata>(DocumentUri);
+    }
+
+    private string GetThumbnailUri()
+    {
+        if(documentMetadata.ThumbnailUri == null) return "images/default.svg";
+        return (documentMetadata.ThumbnailUri.StartsWith('/') ? gitHubService.RawBaseAdddressWithParams : "") + documentMetadata.ThumbnailUri;
+    }
+}
