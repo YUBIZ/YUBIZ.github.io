9897280ac7452083c92f4d7609f2c5fcad095e9c
YUBIZ <k.sein1016@gmail.com>
2025-08-31 00:09:49 +0900

    CategoryView.Document 수정: 정렬 드롭다운 추가 및 문서 정렬, 필터링 로직 추가, 카테고리 필터링 기능을 라우트 매개변수에서 일반 로직으로 전환

--- Blog/Pages/CategoryView/Document.razor.cs
+++ Blog/Pages/CategoryView/Document.razor.cs
@@ -3,15 +3,85 @@ using Blog.Services;
 
 namespace Blog.Pages.CategoryView;
 
+enum OrderType
+{
+    CreateTime,
+    LastUpdateTime,
+}
+
+enum OrderDirection
+{
+    Ascending,
+    Descending
+}
+
 public partial class Document(GitHubService gitHubService)
 {
+    private OrderType _currentOrderType;
+    private OrderType CurrentOrderType
+    {
+        get => _currentOrderType;
+        set
+        {
+            _currentOrderType = value;
+            FilterAndOrderCollection();
+        }
+    }
+
+    private OrderDirection _currentOrderDirection;
+    private OrderDirection CurrentOrderDirection
+    {
+        get => _currentOrderDirection;
+        set
+        {
+            _currentOrderDirection = value;
+            FilterAndOrderCollection();
+        }
+    }
+
+    private string _documentCategory = string.Empty;
+    private string DocumentCategory
+    {
+        get => _documentCategory;
+        set
+        {
+            _documentCategory = value;
+            FilterAndOrderCollection();
+        }
+    }
+
     private FileTree documentFileTree;
 
-    private FilePathWithCommitHistory[]? documentFilePathWithCommitHistoryCollection;
+    private FilePathAndCommitHistory[]? documentFilePathAndCommitHistoryCollection;
+
+    private FilePathAndCommitHistory[]? filteredAndOrderedDocumentFilePathAndCommitHistoryCollection;
 
     protected override async Task OnInitializedAsync()
     {
         documentFileTree = await gitHubService.GetRawFromJsonAsync<FileTree>("DocumentFileTree.json");
-        documentFilePathWithCommitHistoryCollection = await gitHubService.GetRawFromJsonAsync<FilePathWithCommitHistory[]>("DocumentFilePathWithCommitHistoryCollection.json");
+        documentFilePathAndCommitHistoryCollection = await gitHubService.GetRawFromJsonAsync<FilePathAndCommitHistory[]>("DocumentFilePathAndCommitHistoryCollection.json");
+        FilterAndOrderCollection();
+    }
+
+    private void FilterAndOrderCollection()
+    {
+        var collection = documentFilePathAndCommitHistoryCollection?.Where(v => v.FilePath.StartsWith(DocumentCategory));
+
+        Func<FilePathAndCommitHistory, object> orderFunc = CurrentOrderType switch
+        {
+            OrderType.CreateTime => (v => v.CommitHistory.FirstOrDefault().Date),
+            OrderType.LastUpdateTime => (v => v.CommitHistory.LastOrDefault().Date),
+            _ => throw new NotImplementedException()
+        };
+
+        collection = CurrentOrderDirection switch
+        {
+            OrderDirection.Ascending => collection?.OrderBy(orderFunc),
+            OrderDirection.Descending => collection?.OrderByDescending(orderFunc),
+            _ => throw new NotImplementedException()
+        };
+
+
+        filteredAndOrderedDocumentFilePathAndCommitHistoryCollection = collection?.ToArray();
     }
 }
