2b2351e3e836fd021f59fbbe2ee52b5b3665b7df
YUBIZ <k.sein1016@gmail.com>
2025-08-31 02:48:29 +0900

    파일 트리 뷰 업데이트: 파일 보기 기능 추가 및 선택된 요소의 전체 경로를 바인딩 가능하게 하는 로직 추가, 목록 성능 최적화

---
+++ Blog/Components/FileTreeView.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Components;
+
+public partial class FileTreeView
+{
+    private string CurrentDirectoryPath => Path.Combine(ParentPath, FileTree.Value.DirectoryName);
+
+    private async Task OnNameSelectedAsync(string newSelectedName)
+    {
+        if (SelectedName != newSelectedName) await SelectedNameChanged.InvokeAsync(SelectedName = newSelectedName);
+    }
+}
