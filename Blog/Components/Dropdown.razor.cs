e0b6cd400908c37b7264385624bf18ecb9ea7bad
YUBIZ <k.sein1016@gmail.com>
2025-08-31 00:04:40 +0900

    드롭다운 컴포넌트 추가

---
+++ Blog/Components/Dropdown.razor.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.Components;
+
+public partial class Dropdown<T>
+{
+    private async Task OnItemSelectedAsync(T newSelectedItem)
+    {
+        if (!EqualityComparer<T>.Default.Equals(SelectedItem, newSelectedItem)) await SelectedItemChanged.InvokeAsync(SelectedItem = newSelectedItem);
+    }
+}
