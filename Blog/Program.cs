ec5d12494a9a4d966099312daa88f6cc3006d73b
YUBIZ <k.sein1016@gmail.com>
2025-09-02 00:32:07 +0900

    설정 모델 등록 및 변경된 구조에 맞춰 서비스 등록 로직 수정

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,13 +1,17 @@
+﻿using Blog;
+using Blog.Models.Config;
+using Blog.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Blog;
-using Blog.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSingleton(builder.Configuration.Get<AppSettings>() ?? throw new InvalidOperationException("AppSettings를 찾을 수 없습니다."));
+builder.Services.AddSingleton(builder.Configuration.GetSection("GitHubSettings").Get<GitHubSettings>() ?? throw new InvalidOperationException("GitHubSettings를 찾을 수 없습니다."));
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new GitHubService { Owner = "YUBIZ", Repo = "Posts", Ref = "release" });
+builder.Services.AddScoped<GitHubService>();
 
 await builder.Build().RunAsync();
