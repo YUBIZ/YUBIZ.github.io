07b45b116ad954c5dffbc3f0b79a5892d53f89de
YUBIZ <k.sein1016@gmail.com>
2025-09-02 00:29:03 +0900

    Helper를 YamlHelper로 이름 변경

---
+++ Blog/Helpers/YamlHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+using YamlDotNet.Serialization;
+
+namespace Blog.Helpers;
+
+public partial class YamlHelper
+{
+    private static readonly Deserializer deserializer = new();
+
+    public static T? DeserializeYaml<T>(in string input) => deserializer.Deserialize<T>(input);
+
+    public static T? DeserializeYamlFrontMatter<T>(in string input)
+    {
+        Match result = YamlFrontMatterRegex().Match(input);
+        return result.Success ? DeserializeYaml<T>(result.Groups[1].Value) : default;
+    }
+
+    [GeneratedRegex(@"^---(.*?)---", RegexOptions.Singleline)]
+    private static partial Regex YamlFrontMatterRegex();
+}
