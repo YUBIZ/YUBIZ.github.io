f70e829524cf01a89ee3660d9caa3f7100101cdf
YUBIZ <k.sein1016@gmail.com>
2025-09-02 00:30:32 +0900

    YamlHttpClientExtension 확장 메서드 클래스 추가

---
+++ Blog/Misc/HttpClientExtensions/YamlHttpClientExtension.cs
@@ -0,0 +1,32 @@
+﻿using Blog.Helpers;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Blog.Misc.HttpClientExtensions;
+
+public static class YamlHttpClientExtension
+{
+    public static async Task<T?> GetFromYamlAsync<T>(this HttpClient httpClient, [StringSyntax(StringSyntaxAttribute.Uri)] string? requestUri)
+    => YamlHelper.DeserializeYaml<T>(await httpClient.GetStringAsync(requestUri));
+
+    public static async Task<T?> GetFromYamlAsync<T>(this HttpClient httpClient, Uri? requestUri)
+        => YamlHelper.DeserializeYaml<T>(await httpClient.GetStringAsync(requestUri));
+
+    public static async Task<T?> GetFromYamlAsync<T>(this HttpClient httpClient, [StringSyntax(StringSyntaxAttribute.Uri)] string? requestUri, CancellationToken cancellationToken)
+        => YamlHelper.DeserializeYaml<T>(await httpClient.GetStringAsync(requestUri, cancellationToken));
+
+    public static async Task<T?> GetFromYamlAsync<T>(this HttpClient httpClient, Uri? requestUri, CancellationToken cancellationToken)
+        => YamlHelper.DeserializeYaml<T>(await httpClient.GetStringAsync(requestUri, cancellationToken));
+
+    public static async Task<T?> GetFromYamlFrontMatterAsync<T>(this HttpClient httpClient, [StringSyntax(StringSyntaxAttribute.Uri)] string? requestUri)
+        => YamlHelper.DeserializeYamlFrontMatter<T>(await httpClient.GetStringAsync(requestUri));
+
+    public static async Task<T?> GetFromYamlFrontMatterAsync<T>(this HttpClient httpClient, Uri? requestUri)
+        => YamlHelper.DeserializeYamlFrontMatter<T>(await httpClient.GetStringAsync(requestUri));
+
+    public static async Task<T?> GetFromYamlFrontMatterAsync<T>(this HttpClient httpClient, [StringSyntax(StringSyntaxAttribute.Uri)] string? requestUri, CancellationToken cancellationToken)
+        => YamlHelper.DeserializeYamlFrontMatter<T>(await httpClient.GetStringAsync(requestUri, cancellationToken));
+
+    public static async Task<T?> GetFromYamlFrontMatterAsync<T>(this HttpClient httpClient, Uri? requestUri, CancellationToken cancellationToken)
+        => YamlHelper.DeserializeYamlFrontMatter<T>(await httpClient.GetStringAsync(requestUri, cancellationToken));
+
+}
